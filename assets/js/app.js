866fb2395cadb1d7bb99c209820b62ef21edc96f
Milan <puthukkuddymilan@gmail.com>
2020-12-31 14:25:18 +0530

    Added registration form and added footer deatils and contact form

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,65 @@ const closeIcon = document.querySelector('.close-icon');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelector('.nav-links li');
 const paras = document.querySelector('.single-course p');
+const fileName = document.querySelector('.uploaded-file-name');
+
+function processSelectedFile(fileInput) {
+    var files = fileInput.files;
+    
+    fileName.textContent = files[0].name;
+  }
+
+document.querySelectorAll('.call-pop').forEach(element => {
+    element.addEventListener('click', function() {
+        document.getElementById('overlay').classList.add('is-visible');
+        document.getElementById('modal').classList.add('is-visible');
+    });
+});
+
+document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('overlay').classList.remove('is-visible');
+    document.getElementById('modal').classList.remove('is-visible');
+});
+document.getElementById('overlay').addEventListener('click', function() {
+    document.getElementById('overlay').classList.remove('is-visible');
+    document.getElementById('modal').classList.remove('is-visible');
+});
+
+window.addEventListener("load", function() {
+
+	// store tabs variable
+	var myTabs = document.querySelectorAll("ul.nav-tabs > li");
+
+	function myTabClicks(tabClickEvent) {
+
+		for (var i = 0; i < myTabs.length; i++) {
+			myTabs[i].classList.remove("active");
+		}
+
+		var clickedTab = tabClickEvent.currentTarget; 
+
+		clickedTab.classList.add("active");
+
+		tabClickEvent.preventDefault();
+
+		var myContentPanes = document.querySelectorAll(".tab-pane");
+
+		for (i = 0; i < myContentPanes.length; i++) {
+			myContentPanes[i].classList.remove("active");
+		}
+
+		var anchorReference = tabClickEvent.target;
+		var activePaneId = anchorReference.getAttribute("href");
+		var activePane = document.querySelector(activePaneId);
+
+		activePane.classList.add("active");
+
+	}
+
+	for (i = 0; i < myTabs.length; i++) {
+		myTabs[i].addEventListener("click", myTabClicks)
+	}
+});
 
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("open");
@@ -14,8 +73,8 @@ closeIcon.addEventListener("click", () => {
     navLinks.classList.toggle("open");
 });
 
-for (var i = 0; i < document.links.length; i++) {
-    if (document.links[i].href == document.URL) {
-        document.links[i].className = 'active';
-    }
-}
+// for (var i = 0; i < document.links.length; i++) {
+//     if (document.links[i].href == document.URL) {
+//         document.links[i].className = 'active';
+//     }
+// }
