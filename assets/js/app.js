e6759e017b9bb3c2ea49eb96df87e990445736b1
Milan <puthukkuddymilan@gmail.com>
2021-01-04 10:16:56 +0530

    Both Forms updated and ready to use

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,6 +29,9 @@ document.getElementById('overlay').addEventListener('click', function() {
     document.getElementById('overlay').classList.remove('is-visible');
     document.getElementById('modal').classList.remove('is-visible');
 });
+document.getElementById('toast-close-btn').addEventListener('click', function() {
+    document.getElementById('toast').classList.remove('is-visible');
+});
 
 window.addEventListener("load", function() {
 
@@ -72,6 +75,169 @@ hamburger.addEventListener("click", () => {
 closeIcon.addEventListener("click", () => {
     navLinks.classList.toggle("open");
 });
+
+const Url = 'https://homework-hometutors-mailer.herokuapp.com/student';
+const Url2 = 'https://homework-hometutors-mailer.herokuapp.com/teacher';
+
+function submitStudent(e) {
+	e.preventDefault();
+	document.getElementById('student-button').disabled = true;
+	document.getElementById('loader').classList.remove('void');
+	document.getElementById('texter').classList.add('void');
+	let name = document.forms["studForm"]["name"]; 
+	let email = document.forms["studForm"]["email"]; 
+	let phone = document.forms["studForm"]["phone"]; 
+	let classname = document.forms["studForm"]["class"]; 
+	let district = document.forms["studForm"]["district"];
+	var frm = document.getElementsByName('studForm')[0];
+
+	if (name.value == "") { 
+		window.alert("Please enter your name."); 
+		name.focus(); 
+		return false; 
+	} 
+
+	if (email.value == "") { 
+		window.alert( 
+		  "Please enter a valid e-mail address."); 
+		email.focus(); 
+		return false; 
+	} 
+
+	if (phone.value == "") { 
+		window.alert( 
+		  "Please enter your telephone number."); 
+		phone.focus(); 
+		return false; 
+	} 
+
+	if (classname.selectedIndex < 1) { 
+		alert("Please enter your class."); 
+		classname.focus(); 
+		return false; 
+	} 
+	if (district.selectedIndex < 1) { 
+		alert("Please enter your district."); 
+		district.focus(); 
+		return false; 
+	}
+
+	const Data = {
+		name: name.value,
+		email: email.value,
+		phone: phone.value,
+		classname: classname.options[classname.selectedIndex].text,
+		district: district.options[district.selectedIndex].text,
+	}
+	const Params = {
+		headers: {
+			"content-type": "application/json"
+		},
+		body: JSON.stringify(Data),
+		method: "POST"
+	}
+
+	console.log(Data);
+
+	fetch(Url, Params)
+		//.then(data => { return data.json() })
+		.then((res) => {
+				document.getElementById('student-button').disabled = false;
+				document.getElementById('toast').classList.add('is-visible');
+				document.getElementById('overlay').classList.remove('is-visible');
+				document.getElementById('modal').classList.remove('is-visible');
+				document.getElementById('loader').classList.add('void');
+				document.getElementById('texter').classList.remove('void');
+				frm.reset();
+			 })
+		.then((error) => { 
+			document.getElementById('student-button').disabled = false;
+			document.getElementById('loader').classList.add('void');
+			document.getElementById('texter').classList.remove('void');
+			console.log(error) 
+		})
+}
+
+function submitTeacher(e) {
+	e.preventDefault();
+	document.getElementById('teacher-button').disabled = true;
+	document.getElementById('loader2').classList.remove('void');
+	document.getElementById('texter2').classList.add('void');
+	let name = document.forms["techForm"]["name"]; 
+	let email = document.forms["techForm"]["email"]; 
+	let phone = document.forms["techForm"]["phone"]; 
+	let qualification = document.forms["techForm"]["qualification"]; 
+	let address = document.forms["techForm"]["address"];
+	var frm = document.getElementsByName('techForm')[0];
+
+	if (name.value == "") { 
+		window.alert("Please enter your name."); 
+		name.focus(); 
+		return false; 
+	} 
+
+	if (email.value == "") { 
+		window.alert( 
+		  "Please enter a valid e-mail address."); 
+		email.focus(); 
+		return false; 
+	} 
+
+	if (phone.value == "") { 
+		window.alert( 
+		  "Please enter your telephone number."); 
+		phone.focus(); 
+		return false; 
+	} 
+
+	if (qualification.value == "") { 
+		alert("Please enter your qualification."); 
+		qualification.focus(); 
+		return false; 
+	} 
+	if (address.value == "") { 
+		alert("Please enter your address."); 
+		address.focus(); 
+		return false; 
+	}
+	let file = document.getElementById("file-upload").files[0];
+
+	let formData = new FormData();
+
+	formData.append("file", file);
+	formData.append("name", name.value);
+	formData.append("email", email.value);
+	formData.append("phone", phone.value);
+	formData.append("qualification", qualification.value);
+	formData.append("address", address.value);
+
+	const Params = {
+		// headers: {
+		// 	"content-type": "multipart/form-data"
+		// },
+		body: formData,
+		method: "POST"
+	}
+
+	fetch(Url2, Params)
+		//.then(data => { return data.json() })
+		.then((res) => {
+				console.log(res)
+				document.getElementById('teacher-button').disabled = false;
+				document.getElementById('toast').classList.add('is-visible');
+				document.getElementById('overlay').classList.remove('is-visible');
+				document.getElementById('modal').classList.remove('is-visible');
+				document.getElementById('loader2').classList.add('void');
+				document.getElementById('texter2').classList.remove('void');
+				frm.reset();
+			 })
+		.then((error) => { 
+			document.getElementById('teacher-button').disabled = false;
+			document.getElementById('loader2').classList.add('void');
+			document.getElementById('texter2').classList.remove('void');
+			console.log(error) 
+		})
+}
 
 // for (var i = 0; i < document.links.length; i++) {
 //     if (document.links[i].href == document.URL) {
